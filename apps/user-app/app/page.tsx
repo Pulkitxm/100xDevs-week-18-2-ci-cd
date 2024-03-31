7db807caea2c7e3c2fe7998c0de7959e9e31cac3
Pulkit <kpulkit15234@gmail.com>
2024-03-31 19:44:32 +0530

    xazdc

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,6 +4,7 @@ import { authOptions } from "./lib/auth";
 
 export default async function Page() {
   const a=1; 
+  const b=1; 
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
