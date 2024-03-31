b6ef2bbd071911920552bc59c56fe1d47b4616ce
Pulkit <kpulkit15234@gmail.com>
2024-03-31 19:43:01 +0530

    test

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
 export default async function Page() {
+  const a=1; 
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
